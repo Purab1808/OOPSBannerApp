480561b485a709ad2d7f99a96f98f46cb368b535
Purab Vats <purabvats18@gmail.com>
2025-12-29 10:36:55 +0530

    Added UC1 and UC2

---
+++ UC1/PrintOOPS.java
@@ -0,0 +1,7 @@
+package OOPSBanner.UC1;
+
+public class PrintOOPS {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner");
+    }
+}
---
+++ UC2/BannerArray.java
@@ -0,0 +1,20 @@
+package OOPSBanner.UC2;
+
+public class BannerArray {
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "   ***       ***     ******     ****** ",
+                " **   **   **   **  **     **   **      ",
+                "**     ** **     ** **     **   **      ",
+                "**     ** **     ** **     **    *****  ",
+                "**     ** **     **  ******           **",
+                "**     ** **     ** **               ** ",
+                "   ***       ***     **        ******  "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
